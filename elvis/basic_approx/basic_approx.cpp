662f3f08cc0aeb918a54165d65ea5173337e4270
Elvis Stansvik <elvstone@gmail.com>
2013-12-02 20:24:20 +0100

    Cleanups and doc fixes.

--- elvis/basic_approx/basic_approx.cpp
+++ elvis/basic_approx/basic_approx.cpp
@@ -84,8 +84,8 @@ Matrix<uint32_t> primMST(const Matrix<uint32_t>& d) {
         // For each neighbor v of u not already in MST.
         for (size_t v = 0; v < N; ++v) {
             if (0 < d[u][v] && d[u][v] < cost[v] && !inMST[v]) {
-                // Distance from u to v is lower than current cost of v, so mark u
-                // as parent of v and set cost of v to the distance from u to v.
+                // d[u][v] is lower than current cost of v, so mark u
+                // as parent of v and set cost of v to d[u][v].
                 parent[v] = u;
                 cost[v] = d[u][v];
             }
@@ -107,12 +107,12 @@ Matrix<uint32_t> primMST(const Matrix<uint32_t>& d) {
  * @param MST Input minimum spanning tree.
  * @return 2-approximation TSP tour.
  */
-vector<size_t> twoApprox(const Matrix<uint32_t>& MST) {
+vector<uint16_t> twoApprox(const Matrix<uint32_t>& MST) {
     size_t N = MST.size();
 
-    vector<size_t> tour;
+    vector<uint16_t> tour;
     vector<bool> visited(N);
-    stack<int> stack;
+    stack<uint16_t> stack;
     stack.push(0);
 
     while (tour.size() < N) {
@@ -121,7 +121,7 @@ vector<size_t> twoApprox(const Matrix<uint32_t>& MST) {
         if (!visited[u]) {
             tour.push_back(u);
             visited[u] = true;
-            for (size_t v = 0; v < N; ++v) {
+            for (uint16_t v = 0; v < N; ++v) {
                 if (MST[u][v] != 0) {
                     stack.push(v);
                 }
@@ -139,36 +139,18 @@ vector<size_t> twoApprox(const Matrix<uint32_t>& MST) {
  * @return A matrix where row i contains the neighbors of city i
  *         in ascending order of proximity.
  */
-Matrix<uint32_t> createNeighborsMatrix(const Matrix<uint32_t>& d) {
+Matrix<uint16_t> createNeighborsMatrix(const Matrix<uint32_t>& d) {
     size_t N = d.size();
-    Matrix<uint32_t> neighbor(N);
-    for (size_t i = 0; i < N; ++i) {
-        for (size_t j = 0; j < N; ++j) {
+    Matrix<uint16_t> neighbor(N);
+    for (uint16_t i = 0; i < N; ++i) {
+        for (uint16_t j = 0; j < N; ++j) {
             neighbor[i][j] = j;
         }
-        sort(neighbor[i], neighbor[i] + N, [&](size_t j1, size_t j2) {
+        sort(neighbor[i], neighbor[i] + N, [&](uint16_t j1, uint16_t j2) {
             return i != j1 && (i == j2 || d[i][j1] < d[i][j2]);
         });
     }
     return neighbor;
-}
-
-/**
- * Reverse a segment of a tour.
- *
- * This functions reverses the segment [start, end] of the given tour.
- *
- * @param tour The input tour.
- * @param start Start index of segment to reverse.
- * @param end End index of segment to reverse.
- */
-void reverse(vector<size_t> &tour, size_t start, size_t end) {
-    const size_t N = tour.size();
-    while (start < end) {
-        swap(tour[start % N], tour[end % N]);
-        ++start;
-        --end;
-    }
 }
 
 /**
@@ -183,51 +165,23 @@ void reverse(vector<size_t> &tour, size_t start, size_t end) {
  * @param position Vector containing positions of cities in the tour, will
  *                 be updated to reflect the reversal.
  */
-void reverse(vector<size_t> &tour, size_t start, size_t end, vector<size_t>& position) {
+void reverse(vector<uint16_t> &tour, size_t start, size_t end,
+        vector<uint16_t>& position) {
     size_t N = tour.size();
     size_t numSwaps = (((start <= end ? end - start : (end + N) - start) + 1)/2);
-    int i = start;
-    int j = end;
+    uint16_t i = start;
+    uint16_t j = end;
     for (size_t n = 0; n < numSwaps; ++n) {
         swap(tour[i], tour[j]);
         position[tour[i]] = i;
         position[tour[j]] = j;
         i = (i + 1) % N;
-        j = (j - 1 + N) % N;
+        j = ((j + N) - 1) % N;
     }
 }
 
 /**
- * Peforms a 2-opt pass on the given tour.
- *
- * This functions uses the slow and stupid O(N^2) approach.
- *
- * @param tour The tour to optimize.
- * @param d Distance matrix.
- */
-#if 0
-void twoOptSlow(vector<size_t>& tour, const Matrix<uint32_t>& d) {
-    size_t N = tour.size();
-    size_t u, v, w, z;
-    for (size_t i = 0; i < N; ++i) {
-        u = tour[i];
-        v = tour[(i + 1) % N];
-        for (size_t j = i + 2; (j + 1) % N != i; ++j) {
-            w = tour[j % N];
-            z = tour[(j + 1) % N];
-            if (d[u][w] + d[z][v] < d[u][v] + d[w][z]) {
-                //   --u w--        --u-w->
-                //      X     ===>
-                //   <-z v->        <-z-v--
-                reverse(tour, i + 1, j);
-            }
-        }
-    }
-}
-#endif
-
-/**
- * Returns the minimum distance d[i][j], i != j in the given distance matrix.
+ * Returns the shortest distance d[i][j], i != j in the given distance matrix.
  *
  * @param d Distance matrix.
  * @return Minimum distance in d.
@@ -245,13 +199,13 @@ uint32_t minDistance(const Matrix<uint32_t>& d) {
 }
 
 /**
- * Returns the maximum inter-city distance in the given tour.
+ * Returns the longest inter-city distance in the given tour.
  *
  * @param tour Input tour.
  * @param d Distance matrix.
  * @return Maximum inter-city distance in tour.
  */
-uint32_t maxDistance(const vector<size_t>& tour, const Matrix<uint32_t>& d) {
+uint32_t maxDistance(const vector<uint16_t>& tour, const Matrix<uint32_t>& d) {
     size_t N = tour.size();
     uint32_t max = 0;
     for (size_t i = 0, j = 1; i < N; ++i, ++j) {
@@ -266,7 +220,7 @@ uint32_t maxDistance(const vector<size_t>& tour, const Matrix<uint32_t>& d) {
  * @param tour The input tour.
  * @param d Distance matrix.
  */
-uint64_t length(const vector<size_t>& tour, const Matrix<uint32_t>& d) {
+uint64_t length(const vector<uint32_t>& tour, const Matrix<uint32_t>& d) {
     size_t N = tour.size();
     uint64_t length = 0;
     for (size_t i = 0, j = 1; i < N; ++i, ++j) {
@@ -284,32 +238,37 @@ uint64_t length(const vector<size_t>& tour, const Matrix<uint32_t>& d) {
  * @param tour The tour to optimize.
  * @param d Distance matrix.
  * @param neighbor Nearest neighbors matrix.
+ * @param min Shortest possible inter-city distance.
+ * @return true if the tour is 2-opt, otherwise false.
  */
-bool twoOpt(vector<size_t>& tour, const Matrix<uint32_t>& d, const Matrix<uint32_t>& neighbor, uint32_t min) {
+bool twoOpt(vector<uint16_t>& tour, const Matrix<uint32_t>& d,
+        const Matrix<uint16_t>& neighbor, uint32_t min) {
     size_t N = d.size();
-    bool isTwoOpt = true;
+    bool didImprove = false;
 
     // Initialize city tour position vector.
-    vector<size_t> position(N);
-    for (size_t i = 0; i < N; ++i) {
+    vector<uint16_t> position(N);
+    for (uint16_t i = 0; i < N; ++i) {
         position[tour[i]] = i; // tour[i] is the i:th city in tour.
     }
 
-    size_t u, v, w, z;                   // We consider exchanging (u, v) and (w, z).
-    size_t u_i, v_i, w_i, z_i;           // City positions in tour.
+    uint16_t u, v, w, z;
+    size_t u_i, v_i, w_i, z_i;
     uint32_t max = maxDistance(tour, d); // Longest link in current tour.
 
     for (u_i = 0, v_i = 1; u_i < N; ++u_i, ++v_i) {
+        // For each edge (u, v).
         u = tour[u_i];
         v = tour[v_i % N];
         for (size_t n = 0; n < N - 1; ++n) {
+            // Visit nearby edges (w, z).
             w_i = position[neighbor[u][n]];
             z_i = w_i + 1;
             w = tour[w_i]; // w is the n:th closest neighbor of u.
             z = tour[z_i % N];
 
             if (v == w || z == u) {
-                continue;
+                continue; // Skip adjacent edges.
             }
 
             // d[u][w] + min is a lower bound on new length.
@@ -324,17 +283,16 @@ bool twoOpt(vector<size_t>& tour, const Matrix<uint32_t>& d, const Matrix<uint32
                 //   <-z v->        <-z-v--
                 reverse(tour, v_i % N, w_i, position);
                 
-                // FIXME: Update max incrementally.
-                //max = std::max(max, std::max(d[u][w], d[v][z]));
-                max = maxDistance(tour, d);
+                // FIXME: Is this enough to update max?
+                max = std::max(max, std::max(d[u][w], d[v][z]));
 
-                isTwoOpt = false;
+                didImprove = true;
                 break;
             }
         }
     }
 
-    return isTwoOpt;
+    return didImprove;
 }
 
 int main(int argc, char *argv[]) {
@@ -343,15 +301,15 @@ int main(int argc, char *argv[]) {
 
     // Calculate MST and 2-approximation.
     Matrix<uint32_t> MST = primMST(d);
-    vector<size_t> tour = twoApprox(MST);
+    vector<uint16_t> tour = twoApprox(MST);
 
     // Optimize using 2-opt.
-    Matrix<uint32_t> neighbor = createNeighborsMatrix(d);
+    Matrix<uint16_t> neighbor = createNeighborsMatrix(d);
     uint32_t min = minDistance(d);
-    bool isTwoOpt = false;
+    bool didImprove;
     do {
-        isTwoOpt = twoOpt(tour, d, neighbor, min);
-    } while (!isTwoOpt);
+        didImprove = twoOpt(tour, d, neighbor, min);
+    } while (didImprove);
 
     // Print tour.
     for (auto city : tour) {
